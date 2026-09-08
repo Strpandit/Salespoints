08efb3386e343d484d44a96e46d8e3bb3205c2be
Prashant Sharma <prashant100ji@gmail.com>
2026-09-08 21:17:14 +0530

    fix spec  filters

--- app/serializers/cat_filter_serializer.rb
+++ app/serializers/cat_filter_serializer.rb
@@ -1,6 +1,4 @@
 class CatFilterSerializer < ApplicationSerializer
   attributes :id, :name, :data_type, :unit, :options, :is_mandatory, :is_filterable, :display_order, :category_id
-
-  belongs_to :category
 end
 
